69321ab180c1a6459e4ec8b0bd119c0f8d2066de
WilliamHartman <hartman.william@gmail.com>
2022-04-04 22:44:13 -0400

    added build directory path to server

--- server/index.js
+++ server/index.js
@@ -10,11 +10,17 @@ const morgan = require('morgan');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + './../build'));
 app.use(morgan('dev'));
 
 //Endpoints
 app.get(`${baseURL}/autocomplete/`, ctrl.autocomplete)
 app.get(`${baseURL}/autocomplete/:st`, ctrl.autocomplete)
 app.get(`${baseURL}/movie/:id`, ctrl.getMovie)
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
